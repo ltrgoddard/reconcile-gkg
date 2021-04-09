73b38040c53cc25a6d458e6b6ac15b429f26f64e
Louis Goddard <louisgoddard@gmail.com>
2021-04-09 16:46:32 +0100

    Refactoring for style

--- app.py
+++ app.py
@@ -3,6 +3,14 @@ import requests, json, os
 
 app = Flask(__name__)
 
+def make_request(payload, api_key):
+    payload += '&key=' + api_key
+    r = requests.get(payload)
+    if r.status_code == 200:
+        return json.loads(r.text)
+    else:
+        return {'error': r.status_code}
+
 @app.route('/', methods = ['POST'])
 def reconcile():
     queries = request.form.get('queries')
@@ -10,44 +18,37 @@ def reconcile():
     if not queries:
         return jsonify({'error': 'Please provide a batch of queries.'})
     
-    else:
-        queries = json.loads(queries)
-        response = {}
+    queries = json.loads(queries)
+    response = dict.fromkeys(queries.keys(), {})
         
-        # Is there a neater way to do this?
-        for k in queries.keys():
-            response[k] = {}
+    for query_id, query in queries.items():
+        payload = '{endpoint}?query={query}'.format(
+            endpoint = os.environ.get('GKG_ENDPOINT'),
+            query = query['query'])
 
-        for k in queries.keys():
-            payload = '{endpoint}?key={api_key}&query={query}'.format(
-                endpoint = os.environ['GKG_ENDPOINT'],
-                api_key = os.environ['GKG_API_KEY'],
-                query = queries[k]['query'])
-
-            if queries[k].get('type'):
-                payload += '&types=' + ','.join([t for t in queries[k]['type'].split(',')])
+        if query.get('type'):
+            payload += '&types=' + ','.join([t for t in query['type'].split(',')])
             
-            if queries[k].get('limit'):
-                payload += '&limit=' + str(queries[k]['limit'])
+        if query.get('limit'):
+            payload += '&limit=' + str(query['limit'])
 
-            r = requests.get(payload)
-            if r.status_code == 200:
-                results = json.loads(r.text)['itemListElement']
-            else:
-                return jsonify({'error': r.status_code})
+        results = make_request(payload, os.environ.get('GKG_API_KEY'))
 
-            response[k]['result'] = []
+        if results.get('error'):
+            return jsonify(results)
+        
+        response[query_id]['result'] = []
             
-            for item in results:
-                # Exclude result items without a type
-                if item['result'].get('@type'):
-                    response[k]['result'].append({
-                        "id": item['result'].get('@id'),
-                        "name": item['result'].get('name'),
-                        "description": item['result'].get('description'),
-                        "type": [{"id": t, "name": t} for t in item['result'].get('@type')],
-                        "score": item.get('resultScore'),
-                        "match": True
-                    })
+        for item in results['itemListElement']:
+            # Exclude result items without a type
+            if item['result'].get('@type'):
+                response[query_id]['result'].append({
+                    'id': item['result'].get('@id'),
+                    'name': item['result'].get('name'),
+                    'description': item['result'].get('description'),
+                    'type': [{'id': t, 'name': t} for t in item['result'].get('@type')],
+                    'score': item.get('resultScore'),
+                    'match': True
+                })
                 
         return jsonify(response)
